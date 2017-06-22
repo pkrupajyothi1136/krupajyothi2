98b038ed455ca5b1d7228e1a146f57b9da6709bf
pkrupajyothi1136 <pkrupajyothi1136@gmail.com>
2017-06-22 04:03:32 -0700

    Add files via upload

---
+++ ng2-1-GettingStart-20170622T083305Z-001/ng2-1-GettingStart/ng2-2-Component/src/app/ng-content/ng-content.component.ts
@@ -0,0 +1,25 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-ng-content',
+  template: `
+<p>
+    ng-content Works!
+    <ng-content></ng-content>
+</p>
+  `,
+  styles: [`
+p {
+    border: 1px solid red;
+}
+`]
+})
+export class NgContentComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
